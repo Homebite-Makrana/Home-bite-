3037dfff585813716652625429085f519a7349e1
Home Bite <randermd257@gmail.com>
2026-08-19 12:29:53 +0530

    HOME BITE v45 approved old logo blue theme and no-red menu

--- android/app/src/main/java/com/homebite/makrana/MainActivity.java
+++ android/app/src/main/java/com/homebite/makrana/MainActivity.java
@@ -1,16 +1,22 @@
 package com.homebite.makrana;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-
-import androidx.core.splashscreen.SplashScreen;
+import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private static final int HB_NAVY = Color.rgb(7, 11, 20);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        SplashScreen.installSplashScreen(this);
+        getWindow().setBackgroundDrawable(new ColorDrawable(HB_NAVY));
         super.onCreate(savedInstanceState);
+        WebView webView = getBridge() != null ? getBridge().getWebView() : null;
+        if (webView != null) {
+            webView.setBackgroundColor(HB_NAVY);
+        }
     }
 }
